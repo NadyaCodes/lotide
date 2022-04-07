4e65614991c459e736e4629c89f5b30defb116da
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-07 19:25:55 -0400

    updated eqObjects to include else statement...function not updated elsewhere as I want to get an opinion

--- eqObjects.js
+++ eqObjects.js
@@ -17,57 +17,54 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const eqObjects = function(object1, object2) {
+
   const negativeOutput = `👀 👀 👀 Assertion Failed!`;
   const positiveOutput = `😀😀😀 Assertion Passed!`;
 
-  let output = negativeOutput;
-
+  //truth counter
+  let objectTrue = 0;
 
   let keys1 = Object.keys(object1);
   let keys2 = Object.keys(object2);
 
+  //if keys are same length
   if (keys1.length === keys2.length) {
-    let arraysEqual =  false;
 
+    //loop through both sets of keys
     for (let key1 of keys1) {
-      if (Array.isArray(object1[key1])) {
-        let arr1 = object1[key1];
-
-        for (let key2 of keys2) {
+      for (let key2 of keys2) {
+        //if values are arrays, proceed
+        if (Array.isArray(object1[key1])) {
+          let arr1 = object1[key1];
+            
           if (Array.isArray(object2[key2])) {
             let arr2 = object2[key2];
 
-            if (eqArrays(arr1, arr2)) {
-              arraysEqual = true;
-            }
-            if (arraysEqual !== true) {
-              output = negativeOutput;
-              return output;
-            }
-          }
-        }
-      }
-    }
+            //if values match, the truth counter goes up
+            if (eqArrays(arr1, arr2) === false) {
+              return negativeOutput;
 
-    let objectTrue = 0;
-    for (let key1 of keys1) {
-      for (let key2 of keys2) {
-        if (key1 === key2) {
-          if ((object1[key1] === object2[key2]) || arraysEqual) {
+            }
             objectTrue++;
           }
+        } else {
+          //if not an array, and if values match, the counter goes up
+          if (key1 === key2) {
+            if (object1[key1] === object2[key2]) {
+              objectTrue++;
+            }
+          }
         }
       }
-    }
-
-    if (objectTrue === keys1.length) {
-      output = positiveOutput;
+      //at the end, if the counter is the same value as the number of keys, it's good to go!
+      if (objectTrue === keys1.length) {
+        return positiveOutput;
+      }
     }
   }
 
 
-
-  return output;
+  return negativeOutput;
 };
 
 const ab = { a: "1", b: "2" };
@@ -83,3 +80,58 @@ console.log(eqObjects(cd, dc));
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(eqObjects(cd, cd2));
+
+
+// const eqObjects = function(object1, object2) {
+//   const negativeOutput = `👀 👀 👀 Assertion Failed!`;
+//   const positiveOutput = `😀😀😀 Assertion Passed!`;
+
+//   let output = negativeOutput;
+
+
+//   let keys1 = Object.keys(object1);
+//   let keys2 = Object.keys(object2);
+
+//   if (keys1.length === keys2.length) {
+//     let arraysEqual =  false;
+
+//     for (let key1 of keys1) {
+//       if (Array.isArray(object1[key1])) {
+//         let arr1 = object1[key1];
+
+//         for (let key2 of keys2) {
+//           if (Array.isArray(object2[key2])) {
+//             let arr2 = object2[key2];
+
+//             if (eqArrays(arr1, arr2)) {
+//               arraysEqual = true;
+//             }
+//             if (arraysEqual !== true) {
+//               output = negativeOutput;
+//               return output;
+//             }
+//           }
+//         }
+//       }
+//     }
+
+//     let objectTrue = 0;
+//     for (let key1 of keys1) {
+//       for (let key2 of keys2) {
+//         if (key1 === key2) {
+//           if ((object1[key1] === object2[key2]) || arraysEqual) {
+//             objectTrue++;
+//           }
+//         }
+//       }
+//     }
+
+//     if (objectTrue === keys1.length) {
+//       output = positiveOutput;
+//     }
+//   }
+
+
+
+//   return output;
+// };
