925a1844cc8849e5ada6629682f9d6f68b484446
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-11 14:12:08 -0400

    refactored middle to use inputted functions and created a middle test file

--- middle.js
+++ middle.js
@@ -1,51 +1,25 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    let equalCounter = 0;
-
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        equalCounter++;
-      }
-    }
-
-    if (equalCounter === arr1.length) {
-      return true;
-    }
-  }
-  return false;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    return console.log(`🎉🎉🎉 Assertion Passed 🎉🎉🎉 ${arr1} === ${arr2}`);
-  }
-  return console.log(`👀 Nope - ${arr1} !== ${arr2}`);
-  
-};
-
-const middle = function(array) {
+const middle = (array) => {
   const howLong = array.length;
   let middleValue = [];
   const middleIndex = Math.floor((howLong / 2));
 
+  //if input is less or equal to two
   if (howLong <= 2) {
     return middleValue;
   }
 
+  //if input is even
   if (howLong % 2 !== 0) {
     middleValue.push(array[middleIndex]);
     return middleValue;
   }
 
+  //if input isn't even
   middleValue.push(array[middleIndex - 1]);
   middleValue.push(array[middleIndex]);
 
   return middleValue;
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
+
